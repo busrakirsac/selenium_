9e7cfc231d5ec73e26f25f2aec923e22e73173dd
buusra <busrakirsac@gmail.com>
2024-01-06 17:21:37 +0300

    v1

---
+++ src/test/java/day02_drivermethods/C01.java
@@ -0,0 +1,17 @@
+package day02_drivermethods;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://amazon.com");
+
+
+    }
+}
